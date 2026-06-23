8f5d177b02873e5e565ee64fb4cda33bf25434e6
Riccardo Papa <rickypapa2006@gmail.com>
2026-06-23 19:37:47 +0200

    Delete MoodTracker directory

--- MoodTracker/analytics/streak.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "../mood_tracker.h"
-
-// Compute current streak and longest streak into app->stats
-void mood_streak_compute(MoodTrackerApp* app);
--- MoodTracker/gui/stats.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "../mood_tracker.h"
-
-void* stats_view_alloc(MoodTrackerApp* app);
-void  stats_view_free(MoodTrackerApp* app);
-void  stats_view_refresh(MoodTrackerApp* app);
